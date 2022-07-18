a5660f4a393a3907acb3b7adad12e4b72efa5e79
michael-duke <michael.21duke@gmail.com>
2022-07-18 16:50:23 +0300

    Add close feature when the user presses outside the mobile-menu

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ menu.addEventListener('click', () => {
   navbar.classList.toggle("nav-toggle");
 });
 
+navbar.addEventListener('click', (event)=>{
+  const {target} = event
+  if(target.nodeName==='NAV' || target.nodeName==='UL')
+  navbar.classList.remove('nav-toggle');
+})
 window.addEventListener("scroll" , () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("nav-toggle");
