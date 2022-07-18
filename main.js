d2d46859b98daefe0b63a67833509c2e027daf52
Peter Njuguna <Ptahbn@gmail.com>
2022-07-18 18:44:21 +0300

    fixed indentation

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-const menu = document.querySelector("#icon");
-const navbar = document.querySelector("nav");
+const menu = document.querySelector('#icon');
+const navbar = document.querySelector('nav');
 menu.addEventListener('click', () => {
-  menu.classList.toggle("fa-times");
-  navbar.classList.toggle("nav-toggle");
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('nav-toggle');
 });
 
-navbar.addEventListener('click', (event)=>{
-  const {target} = event
-  if(target.nodeName==='NAV' || target.nodeName==='UL')
+navbar.addEventListener('click', (event) => {
+  const { target } = event;
+  if (target.nodeName === 'NAV' || target.nodeName === 'UL') { navbar.classList.remove('nav-toggle'); }
+});
+window.addEventListener('scroll', () => {
+  menu.classList.remove('fa-times');
   navbar.classList.remove('nav-toggle');
-})
-window.addEventListener("scroll" , () => {
-  menu.classList.remove("fa-times");
-  navbar.classList.remove("nav-toggle");
 });
